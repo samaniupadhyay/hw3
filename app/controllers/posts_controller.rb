3e9b694254d96aa15906f1c14031456b6a044b7a
samaniupadhyay <samani.upadhyay@kellogg.northwestern.edu>
2023-02-21 00:30:17 +0000

    redirect

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ def create
 @post["posted_on"] = params["post"]["posted_on"]
 @post["place_id"] = params["post"]["place_id"]
 @post.save
-redirect_to "/places"
+redirect_to "/places/#{@post["place_id"]}"
 end
 
 end
